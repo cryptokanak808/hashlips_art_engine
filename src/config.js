61dcb39c9991193214a5eee2c169ece2c45c12b5
cryptokanak808 <cryptokanak808@gmail.com>
2022-02-18 16:44:42 -0800

    uptade

--- src/config.js
+++ src/config.js
@@ -24,15 +24,16 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 5000,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Background Colors" },
+      { name: "Background Fades" },
+      { name: "body color" },
+      { name: "Outline bottom color" },
+      { name: "Tops" },
+      { name: "Outline" },
+      { name: "Mouth" },
+      { name: "Eyes" },
     ],
   },
 ];
